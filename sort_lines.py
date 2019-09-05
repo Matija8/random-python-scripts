d87f258a0e564259e7debf88288bcd5ef8ec11a1
Matija8 <matijanme@gmail.com>
2019-09-05 23:08:45 +0200

    Added sort_lines.py

---
+++ sort_lines.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import sys
+
+def main():
+    #print("Starting...")
+    args = sys.argv
+    if(len(args) < 2):
+        exit("Provide more arguments!")  
+    
+    for file in args[1:]:
+        #print(f"Reading: {file}...")
+        try:
+            with open(file, "r") as input:
+                input = sorted(input)
+                write_to_file(make_out_name(file), input)
+        except FileNotFoundError as e:
+            print("Input file: {file} not found. Please confirm file name exists.")
+    print("All done!")
+    return
+
+
+def write_to_file(out_file, lines):
+    #print(f"Writing to: {out_file}...")
+    try:
+        with open(out_file, "x") as out:
+            for line in lines:
+                out.write(line.strip() + '\n')
+    except FileExistsError as e:
+        print(f"File {out_file} exists already! No changes commited.")
+    return
+
+
+def make_out_name(in_name):
+    pos = in_name.rfind(".")
+    out_name = in_name[0:pos] + ' - sorted.' + in_name[pos+1:]
+    return out_name
+
+
+if __name__ == "__main__":
+    main()
