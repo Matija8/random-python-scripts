06fa5c68420c1aeb6d6be307649b9c932ba904a4
Matija8 <matijanme@gmail.com>
2019-10-07 03:13:34 +0200

    Preuredjen kod. Enumeracija sad vazi i za broj linija >100 (<10)

--- enumerate_lines.py
+++ enumerate_lines.py
@@ -2,36 +2,57 @@
 # encoding: UTF-8
 
 import sys
+set_encoding = "UTF-8"
 
 def main():
-    set_encoding = "UTF-8"
     lines = []
     for file_name in sys.argv[1:]:
-        try:
-            with open(file_name, "r+", encoding = set_encoding) as file:
-                lines = file.readlines()
-        except IOError as e:
-            print(e)
-            return None
-        print(lines)
-        try:
-            with open(file_name, "w+", encoding = set_encoding) as file:
-                i = 1
-                for line in lines:
-                    if i < 10:
-                        line_number = '0' + str(i)
-                    else:
-                        line_number = str(i)
-                    line = line_number + ' ' + line
-                    file.write(line)
-                    i += 1
-                file.flush()
-        except IOError as e:
-            print(e)
-            return None
+        lines = get_lines(file_name)
+        if lines == None:
+            return
+        write_with_line_numbers(file_name, lines)
+    return
 
 
-    
+def get_lines(file_name = ""):
+    try:
+        with open(file_name, "r+", encoding = set_encoding) as file:
+            lines = file.readlines()
+    except IOError as e:
+        print(e)
+        return None
+    return lines
+
+
+def write_with_line_numbers(file_name = "", lines = []):
+    try:
+        with open(file_name, "w+", encoding = set_encoding) as file:
+            i = 1
+            digit_length = line_number_length(len(lines))
+            for line in lines:
+                line_number = fill_line_number(i, digit_length)
+                line = line_number + ' ' + line
+                file.write(line)
+                i += 1
+            file.flush()
+    except IOError as e:
+        print(e)
+    return
+
+
+def line_number_length(number_of_lines):
+    return len(str(number_of_lines))
+
+
+#da bude mozda metoda klase line_num?
+#optimizacija?
+def fill_line_number(i, length):
+    i = str(i)
+    while len(i) < length:
+        i = '0' + i
+    return i
+
+
 
 if __name__ == "__main__":
     main()
