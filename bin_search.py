1363526721fff7b0c20c5e07a1aa5cf68c8471a9
matija8 <matijanme@gmail.com>
2020-05-05 03:02:02 +0200

    Added test array creation and json saving.

--- bin_search.py
+++ bin_search.py
@@ -5,9 +5,10 @@
 from random import randint
 from timeit import default_timer as timer
 from typing import List
+import json
 
 
-def _lin_search(arr, x):
+def _lin_search(arr: List[float], x: float) -> (int, int):
     searches = 0
     for i in range(len(arr)):
         searches += 1
@@ -16,7 +17,7 @@ def _lin_search(arr, x):
     return (-1, searches)
 
 
-def bin_search(arr: List[float], x: float, reverse=False):
+def bin_search(arr: List[float], x: float, reverse=False) -> (int, int):
     searches = 0
     l = -1
     r = len(arr)
@@ -40,18 +41,42 @@ def _rand_arr_gen(length, lower=0, upper=100000000):
         yield randint(lower, upper)
 
 
-def _main():
-    print('Creating a test array...')
-    length = 1000000
-    test_arr = [x for x in _rand_arr_gen(length)]
+def _create_test_array(length=1000000, read_json=False, save_json=False, filename='array.json') -> (List[int], int):
+    """ Create (or read) a sorted array of integers """
+    if read_json:
+        print('Reading the test array from json...')
+        test_arr = _read_array_from_json(filename)
+    else:
+        print('Creating a test array...')
+        test_arr = [x for x in _rand_arr_gen(length)]
     print('Sorting the test array...')
     test_arr.sort()
-    # print(f'test array = \n{test_arr}')
+    if save_json:
+        print('Saving array to json...')
+        _save_array_to_json(test_arr, filename)
     print('Picking random element...')
     x_index = randint(0, length-1)
     x = test_arr[x_index]
     print(f'\tx = array[{x_index}] = {x}')
+    return (test_arr, x)
 
+
+def _save_array_to_json(array: List[int], filename='array.json') -> None:
+    with open(filename, 'w') as out:
+        json.dump(array, out)
+
+
+def _read_array_from_json(filename='array.json') -> List[int]:
+    try:
+        with open(filename, 'r') as inp:
+            array = json.load(inp)
+            return array
+    except Exception:
+        print('Exception in _read_array_from_json')
+
+
+def _main() -> None:
+    test_arr, x = _create_test_array(10000)
     start = timer()
     linear = _lin_search(test_arr, x)
     lin_time = timer() - start
